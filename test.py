d30924a6c51bfc3b864b655e641c60d3a233cf19
LauraMattz <114192652+LauraMattz@users.noreply.github.com>
2022-11-11 19:21:04 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 import streamlit as st
-# import networkx for graph generation
-import networkx as nx
-  
-# import matplotlib library
-import matplotlib.pyplot as plt
-
-# import xlrd for excel reading
-import xlrd
 
 st.title('Mapa de Redes')
 
