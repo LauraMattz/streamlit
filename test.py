22a42cb2bdabe70ff45c1bcaf576cc07a1473a78
LauraMattz <114192652+LauraMattz@users.noreply.github.com>
2022-11-11 19:22:45 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import networkx as nx
+import matplotlib.pyplot as plt
+import xlrd
 
 st.title('Mapa de Redes')
 
+xlrd.xlsx.ensure_elementtree_imported(False, None)
+xlrd.xlsx.Element_has_iter = True
+
+file = r"C://Users/lmattos/Downloads/teste.xlsx"
+G = nx.Graph()
+names = []
+total_names = []
+
+book = xlrd.open_workbook(file)
+sheet = book.sheet_by_index(0)
+
+for row in range(sheet.nrows):
+    data= sheet.row_slice(row)
+    person1 = data[0].value
+    person2 = data[1].value
+    names.append((person1, person2))
+    total_names.append(person1)
+    total_names.append(person2)
+    
+node_sizes = [(total_names.count(node)*100) for node in G.nodes()]
+
+
+G.add_edges_from(names)
+
+pos = nx.circular_layout(G, scale=5)
+
+nx.draw(G, with_labels=True)
+options = {"node_size": 1200, "node_color": "r"}
+
+plt.show()
