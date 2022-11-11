72c464a0e46c2b33dcae5887c850e729fc83c4fa
LauraMattz <114192652+LauraMattz@users.noreply.github.com>
2022-11-11 17:21:14 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+#pandas
+import pandas as pd
+from pandasql import sqldf
 
+doar= pd.read_excel("C://Users/lmattos/Downloads/teste.xlsx", sheet_name="DOAR")
+receber= pd.read_excel("C://Users/lmattos/Downloads/teste.xlsx", sheet_name="RECEBER")
+pessoas= pd.read_excel("C://Users/lmattos/Downloads/teste.xlsx", sheet_name="PESSOAS")
+
+q="""
+select nome, doar,receber from doar
+left join pessoas on doar.ID_QUEM = pessoas.ID
+left join receber on receber.ID_QUEM = pessoas.ID
+where doar = 'FUTEBOL'
+"""
+
+print(sqldf(q))
