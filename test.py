b3aed3a2c101ef293219092daa95fe8555f3b40e
LauraMattz <114192652+LauraMattz@users.noreply.github.com>
2022-11-12 17:43:56 -0300

    Update test.py

--- test.py
+++ test.py
@@ -62,6 +62,30 @@ st.pyplot(plt)
 
 
 
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
+st.title("Auto Filter Dataframes in Streamlit")
+st.write(
+    """This app accomodates the blog [here](<https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/>)
+    and walks you through one example of how the Streamlit
+    Data Science Team builds add-on functions to Streamlit.
+    """
+)
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    <insert the code we wrote here>
+
+data_url = "https://github.com/LauraMattz/streamlit/blob/main/teste.xlsx"
+
+df = pd.read_csv(data_url)
+st.dataframe(filter_dataframe(df))
 
 
