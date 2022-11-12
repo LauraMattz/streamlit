e5fec1d5b18b505bb8a5dbf6006f61ac72fac0a0
LauraMattz <114192652+LauraMattz@users.noreply.github.com>
2022-11-12 19:46:49 -0300

    Update test.py

--- test.py
+++ test.py
@@ -58,7 +58,7 @@ def filter_dataframe(excel_data_df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Filtered dataframe
     """
-    modify = st.checkbox(label='Filtro', value=False, key='test')
+    modify = st.a_checkbox(label='Filtro', value=False, key='test')
 
     if not modify:
         return df 
